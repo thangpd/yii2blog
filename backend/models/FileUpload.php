d076bda1344add4f86ffe2c298604808055e0029
Thang <phamdinhthang1412@gmail.com>
2019-09-26 14:10:57 +0700

    create unique filename function

--- backend/models/FileUpload.php
+++ backend/models/FileUpload.php
@@ -12,23 +12,70 @@ use yii\web\UploadedFile;
 
 class FileUpload extends Model {
 
-	public function uploadFile( $model,$attribute, $path ) {
+	private $path;
 
-		$file = UploadedFile::getInstance( $model, $attribute);
+	public function __construct() {
+		$this->path = \Yii::$app->basePath;
+	}
+
+	public function uploadFile( $model, $attribute, $path = '/web/uploads/' ) {
+
+		$file = UploadedFile::getInstance( $model, $attribute );
 
 		if ( ! empty( $file ) ) {
-			$res = $file->saveAs( $path . $file->name );
-			if ( ! $res ) {
-				\Yii::$app->session->addFlash( 'error', "Can't upload image" . $file->name );
-			} else {
-				\Yii::$app->session->addFlash( 'success', "Uploaded image succeed" );
+			$file->name = str_replace( ' ', '_', $file->name );
+			$file->name = $this->uniqueFileName( $file->name );
 
-				return Url::to( '@web/uploads/' . $file->name, true );
+			if ( ! empty( $file->name ) ) {
+				$res = $file->saveAs( $this->path . $path . $file->name );
+				if ( ! $res ) {
+					\Yii::$app->session->addFlash( 'error', "Can't upload image" . $file->name );
+				} else {
+					\Yii::$app->session->addFlash( 'success', "Uploaded image succeed" );
+
+					return Url::to( '@web/uploads/' . $file->name, true );
+				}
+
 			}
-
 		}
 
 		return '';
+	}
+
+	/**
+	 * Check if file name exists and add more suffix number
+	 *
+	 */
+
+	public function uniqueFileName( &$file_name, $path = '/web/uploads/', $level = 1 ) {
+
+		$checkPath = $this->path . $path;
+		if ( file_exists( $checkPath . $file_name ) ) {
+			$tmp = $file_name;
+			$tmp = preg_replace( '#(.*?)\.#', '${1}_' . $level . '.', $tmp );
+			if ( file_exists( $checkPath . $tmp ) ) {
+				$this->uniqueFileName( $file_name, $path, $level + 1 );
+			} else {
+				$file_name = $tmp;
+
+				return $file_name;
+			}
+		}
+
+		return $file_name;
+	}
+
+	/**
+	 * Delete image when blog is deleted or updated image
+	 *
+	 * @param integer $id
+	 *
+	 * @return boolean
+	 */
+	public static function deleteFile( $model, $attribute, $path = '@web/uploads/' ) {
+
+
+		return false;
 	}
 
 
